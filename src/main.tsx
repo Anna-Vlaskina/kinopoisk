3799fc92af6849954d0d119eb487f28f6c90d9fd
Warno <anna.nikodimovna@yandex.ru>
2026-05-23 00:50:19 +0700

    chore: setup global styles and assets

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app/App.tsx';
-import './app/styles/globals.scss';
+import './app/styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
