4551d8501bfd01f9f33b206b6384ef32e8905796
Warno <anna.nikodimovna@yandex.ru>
2026-06-02 16:53:46 +0700

    feat(pages): add HomePage component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,21 @@
-import { mockSections } from "@/shared/mocks/mockSection";
-import Header from "@/widgets/header";
-import MoviesCatalog from "@/widgets/movies-catalog";
+import HomePage from "@/pages/home";
 
 function App() {
   return (
-    <div
-      style={{
-        display: "Flex",
-        flexDirection: "column",
-        gap: "50px",
-        backgroundColor: "var(--color-background)",
-        fontFamily: "var(--font-family-main)",
-        width: "100%",
-        height: "100%",
-        padding: "30px 100px",
-        boxSizing: "border-box",
-      }}
-    >
-      <Header></Header>
-      <MoviesCatalog sections={mockSections}></MoviesCatalog>
-    </div>
+    // <div
+    //   style={{
+    //     display: "Flex",
+    //     flexDirection: "column",
+    //     gap: "50px",
+    //     backgroundColor: "var(--color-background)",
+    //     fontFamily: "var(--font-family-main)",
+    //     width: "100%",
+    //     height: "100%",
+    //     padding: "30px 100px",
+    //     boxSizing: "border-box",
+    //   }}
+    // ></div>
+    <HomePage></HomePage>
   );
 }
 
---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,25 @@
+import Header from "@/widgets/header";
+import HeroSection from "@/widgets/hero-section";
+import PremieresCarousel from "@/widgets/premieres-carousel";
+import MoviesCatalog from "@/widgets/movies-catalog";
+import type { FC } from "react";
+import { mockMovies } from "@/shared/mocks/mockMovies";
+import { mockSections } from "@/shared/mocks/mockSection";
+import styles from "./HomePage.module.css";
+
+const HomePage: FC = () => {
+  return (
+    <div className={styles.page}>
+      <Header></Header>
+      <main className={styles.catalog}>
+        <section className={styles.hero}>
+          <HeroSection></HeroSection>
+          <PremieresCarousel movies={mockMovies}></PremieresCarousel>
+        </section>
+        <MoviesCatalog sections={mockSections}></MoviesCatalog>
+      </main>
+    </div>
+  );
+};
+
+export default HomePage;
