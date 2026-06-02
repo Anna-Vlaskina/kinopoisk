005983ea039435e0d5bbf9107e9472a6485892b6
Warno <anna.nikodimovna@yandex.ru>
2026-06-02 16:55:54 +0700

    refactor(app): add HomePage component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,7 @@
 import HomePage from "@/pages/home";
 
 function App() {
-  return (
-    // <div
-    //   style={{
-    //     display: "Flex",
-    //     flexDirection: "column",
-    //     gap: "50px",
-    //     backgroundColor: "var(--color-background)",
-    //     fontFamily: "var(--font-family-main)",
-    //     width: "100%",
-    //     height: "100%",
-    //     padding: "30px 100px",
-    //     boxSizing: "border-box",
-    //   }}
-    // ></div>
-    <HomePage></HomePage>
-  );
+  return <HomePage></HomePage>;
 }
 
 export default App;
